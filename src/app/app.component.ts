ffbe21350e78b94dc158c8fabc5f3607d9bb80e9
Misha Martin <martimi@Mac.lan>
2025-02-21 17:12:02 -0700

    12. Event Binding Syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'password-generator';
+  onButtonClick() {
+    console.log('Button was clicked');
+  }
 }
