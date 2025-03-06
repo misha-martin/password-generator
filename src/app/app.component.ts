12e5e0dacb344effa5f619b5c7ec42c757a9cebe
misha-martin <mishanicolemartin@gmail.com>
2025-03-06 14:38:18 -0700

    Lesson 23: Added Bulma Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,66 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  onButtonClick() {
-    console.log('Button was clicked');
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password=''; 
+
+  onChangeLength(event: Event) {
+    const target = (event.target as HTMLInputElement)?.value;
+    const parsedValue = parseInt(target);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+
   }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
 }
