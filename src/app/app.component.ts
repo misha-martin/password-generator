308c1b83d97892d6f5687ec9caeaae2d5c2a5235
misha-martin <mishanicolemartin@gmail.com>
2025-03-06 14:47:32 -0700

    Lesson 24: Structural Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
